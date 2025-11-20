e3ad5f588579d7e4030623e0a4990397c4a8356a
André Félix <afelixfreitas@gmail.com>
2025-11-20 18:50:49 -0300

    refactor(payment-domain): add filters to payment list

--- src/modules/payment/domain/repositories/payment.repository.ts
+++ src/modules/payment/domain/repositories/payment.repository.ts
@@ -1,8 +1,12 @@
-import { Payment } from "../../domain/entities/payment.entity";
+import { Payment } from "../entities/payment.entity";
 
 export interface PaymentRepository {
   create(payment: Payment): Promise<Payment>;
   update(payment: Payment): Promise<Payment>;
   findById(id: string): Promise<Payment | null>;
-  list(): Promise<Payment[]>;
+  list(filters: {
+    cpf?: string;
+    method?: string;
+    status?: string;
+  }): Promise<Payment[]>;
 }
